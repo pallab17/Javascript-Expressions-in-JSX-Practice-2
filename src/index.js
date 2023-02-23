0ddd4279f6374e287d4508d5857990d6b438b366
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-23 16:07:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+const YOURNAME="pb7";
+const CURRENTYEAR=2023;
 //It should display 2 paragraph HTML elements.
+ReactDOM.render(
+  <div>
+<p> Created by {YOURNAME}  </p>
+<p> Copyright {CURRENTYEAR}</p>
+
+  </div>,
+  document.getElementById("root")
+);
 //The paragraphs should say:
 //Created by YOURNAME.
 //Copyright CURRENTYEAR.
